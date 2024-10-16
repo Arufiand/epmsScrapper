ff0e2732e8620b946984054bb206644b34a1fae5
Alfian Danny Armanta <alfiandannyarmanta@gmail.com>
2024-10-16 09:24:44 +0700

    feat: update PQA Scrapper module
    Create module PQA Scrapper
    
    Closes: #16

---
+++ src/pqa/index.js
@@ -0,0 +1,36 @@
+const { dateCreator, jsonFormatter, delay } = require("../general.module");
+
+async function pqaFetcher(page, URL, REPORT) {
+    await page.goto(`${process.env.URL}${URL}`, { waitUntil: 'networkidle0' });
+
+    // Ensure dateCreator returns a properly formatted date like 'YYYY-MM-DD'
+    const formattedDate = dateCreator(); // Assuming it returns in 'YYYY-MM-DD' format
+
+    // Directly set the values of date_from and date_to using evaluate
+    await page.evaluate((formattedDate) => {
+        document.querySelector('input[id="date_from"]').value = formattedDate;
+        document.querySelector('input[id="date_to"]').value = formattedDate;
+    }, formattedDate);
+
+    // Wait for the date to be set and submit to be clicked
+    await Promise.all([
+        page.click(`text=Submit`),
+        page.waitForResponse(response => response.ok() && response.url().includes('/pqa/sectionhead/get_workplan')) // Adjust this URL as needed
+    ]);
+
+    // Add a short delay to ensure all data is loaded
+    await delay(3000);
+
+    // Process the result data
+    await jsonFormatter(page, REPORT);
+}
+
+async function masterPqaFetch(page) {
+    await pqaFetcher(page, process.env.DAILY_REPORT_PQA_RKH_URL, process.env.DAILY_REPORT_PQA_RKH);
+    await delay(500); // Wait for half a second between fetches
+    await pqaFetcher(page, process.env.DAILY_REPORT_PQA_TALLY_SHEET_URL, process.env.DAILY_REPORT_PQA_TALLY_SHEET);
+}
+
+module.exports = {
+    masterPqaFetch,
+};
